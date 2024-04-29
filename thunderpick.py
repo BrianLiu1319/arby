a5580da4aefe1e39816ef1044d6f48b91b9e795c
unknown <brianliu1337@yahoo.com>
2024-04-29 08:57:30 -0700

    tp update

--- thunderpick.py
+++ thunderpick.py
@@ -5,20 +5,57 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 
+def update_tp_html():
+    driver = webdriver.Chrome()
+    url = "https://thunderpick.io/esports/valorant"
+    driver.get(url)
 
-driver = webdriver.Chrome()
-url = "https://thunderpick.io/esports/valorant"
-driver.get(url)
+    driver.implicitly_wait(5)
 
-driver.implicitly_wait(5)
+    html_source =  driver.page_source
 
-html_source =  driver.page_source
+    html_source_code = driver.execute_script("return document.body.innerHTML;")
+    soup = BeautifulSoup(html_source_code, 'html.parser')
 
-html_source_code = driver.execute_script("return document.body.innerHTML;")
-soup = BeautifulSoup(html_source_code, 'html.parser')
+    with open("thunderpick.html", "a", encoding="utf-8") as file:
+            file.write(soup.prettify())
+            
+# # def parse_rivalry():
+#     with open("rivalry.html", "r", encoding="utf-8") as file:
+#         contents = file.read()
+#         soup = BeautifulSoup(contents, "html.parser")
+#         bets = [i for i in soup.find_all(class_="betline m-auto betline-wide mb-0")]
 
-with open("thunderpick.html", "a", encoding="utf-8") as file:
-        file.write(soup.prettify())
+#     rivalry_parsed = dict()
+#     for i in bets:
+#         # [BLEED, PRX]
+#         teams = [
+#             " ".join(j.get_text().split()) for j in i.find_all(class_="outcome-name")
+#         ]
+#         # [5.0, 1.9]
+#         odds = [
+#             " ".join(j.get_text().split()) for j in i.find_all(class_="outcome-odds")
+#         ]
+#         # Apr 27 08:00 UTC
+#         time = " ".join(
+#             i.find(class_="text-navy dark:text-[#CFCFD1] leading-3 text-[11px]")
+#             .get_text()
+#             .split()
+#         )
+#         # {BLEED: 5.0, PRX:1.9}
+#         bundle = dict(zip(teams, odds))
+#         rivalry_parsed[time] = bundle
+
+#     return rivalry_parsed
+
+
+with open("thunderpick.html", "r", encoding="utf-8") as file:
+    contents = file.read()
+    soup = BeautifulSoup(contents, "html.parser")
+    
+a = soup.find_all(class_='match-group')
+
+print(len(a))
 
 
 
